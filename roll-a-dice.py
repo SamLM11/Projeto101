32061cef10d1a0116fbcf937bc3b562e0ce5c43c
SamLM11 <samuellm2011@gmail.com>
2023-11-29 16:18:46 -0300

    first commit

---
+++ roll-a-dice.py
@@ -0,0 +1,54 @@
+import random
+
+response = 'y'
+
+while response == "y":
+    no = random.randint(1,6)
+
+if no == 1:
+    print('[-----]')
+    print('[     ]')
+    print('[  1  ]')
+    print('[     ]')
+    print('[-----]')
+    input("Digite 'y' para continuar ou 'n' para sair")
+
+if no == 2:
+    print('[-----]')
+    print('[     ]')
+    print('[  2  ]')
+    print('[     ]')
+    print('[-----]')
+    input("Digite 'y' para continuar ou 'n' para sair")
+
+if no == 3:
+    print('[-----]')
+    print('[     ]')
+    print('[  3  ]')
+    print('[     ]')
+    print('[-----]')
+    input("Digite 'y' para continuar ou 'n' para sair")
+
+if no == 4:
+    print('[-----]')
+    print('[     ]')
+    print('[  4  ]')
+    print('[     ]')
+    print('[-----]')
+    input("Digite 'y' para continuar ou 'n' para sair")
+
+if no == 5:
+    print('[-----]')
+    print('[     ]')
+    print('[  5  ]')
+    print('[     ]')
+    print('[-----]')
+    input("Digite 'y' para continuar ou 'n' para sair")
+
+if no == 6:
+    print('[-----]')
+    print('[     ]')
+    print('[  6  ]')
+    print('[     ]')
+    print('[-----]')
+    input("Digite 'y' para continuar ou 'n' para sair")
